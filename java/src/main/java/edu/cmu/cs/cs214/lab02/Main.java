5814405e515865abfc666e13d49fd5d304fb6c8c
yantong1775 <yantong1775@gmail.com>
2024-09-06 08:32:21 +0800

    fix: implementing the Shape interface and update Renderer to use Shape interface

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,18 @@
 package edu.cmu.cs.cs214.lab02;
-
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(1);
+        Shape square = new Square(2);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer rectangleRenderer = new Renderer(rectangle);
+        Renderer circleRenderer = new Renderer(circle);
+        Renderer squareRenderer = new Renderer(square);
 
-        renderer.draw();
+        rectangleRenderer.draw();
+        circleRenderer.draw();
+        squareRenderer.draw();
     }
 }
--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -3,14 +3,14 @@ package edu.cmu.cs.cs214.lab02;
 import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Renderer {
-    public Rectangle rectangle;
+    public Shape object;
     
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+    Renderer(Shape object) {
+        this.object = object;
     }
 
     void draw() {
-        double area = rectangle.getArea();
+        double area = object.getArea();
 
         // assume implementation
 
